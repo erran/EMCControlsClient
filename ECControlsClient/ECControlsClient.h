7b1c10d9d71709847c0b015cd611983dd7c93d75
Erran Carey <me@errancarey.com>
2014-01-03 23:02:49 -0600

    Create ECControlsClient.h and ECControlsClient.m

---
+++ ECControlsClient/ECControlsClient.h
@@ -0,0 +1,13 @@
+//
+//  ECControlsClient.h
+//  
+//
+//  Created by Erran Carey on 1/3/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ECControlsClient : NSObject
+
+@end
---
+++ ECControlsClient/ECControlsClient.m
@@ -0,0 +1,13 @@
+//
+//  ECControlsClient.m
+//  
+//
+//  Created by Erran Carey on 1/3/14.
+//
+//
+
+#import "ECControlsClient.h"
+
+@implementation ECControlsClient
+
+@end
