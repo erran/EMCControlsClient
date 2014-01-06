4eef8504d51d27d0bb05a7c14ca5006af7ceaf01
Erran Carey <me@errancarey.com>
2014-01-05 23:36:19 -0600

    Create Podfile
    + Generate Xcode project

---
+++ Example/EMCControlsClientDemo/EMCControlsClientDemo/EMCControlsClientDemo/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  EMCControlsClientDemo
+//
+//  Created by Erran Carey on 1/5/14.
+//  Copyright (c) 2014 Erran Carey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
+
